2c39f3e3c95ec9e5c59d0c42136a01bd2efa082e
Kacper <61354086+Kacpercio@users.noreply.github.com>
2025-07-04 12:56:06 +0200

    Update ProcessVendorPayment.php

--- app/Jobs/ProcessVendorPayment.php
+++ app/Jobs/ProcessVendorPayment.php
@@ -1,1 +1,28 @@
+<?php
+namespace App\Jobs;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Orders;
+
+class ProcessVendorPayment implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+
+    public function __construct(Orders $order)
+    {
+        $this->order = $order;
+    }
+
+    public function handle()
+    {
+        if (!$this->order->vendor_payment_amount) {
+            $this->order->processVendorPayment();
+        }
+    }
+}
